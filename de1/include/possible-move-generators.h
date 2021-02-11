91cacbd57832bea2466357921d2babf4a26a78fd
Madeline Ferguson <lusul101@gmail.com>
2021-02-11 15:52:10 -0800

    Stub for pawn move generator

---
+++ de1/include/possible-move-generators.h
@@ -0,0 +1,3 @@
+#include "game.h"
+
+board_t **generate_pawn_moves(board_t *current, int pawn);
---
+++ de1/possible-move-generators.c
@@ -0,0 +1,7 @@
+#include "include/possible-move-generators.h"
+#include <stdlib.h>
+
+board_t **generate_pawn_moves(board_t *current, int pawn)
+{
+    return NULL;
+}
