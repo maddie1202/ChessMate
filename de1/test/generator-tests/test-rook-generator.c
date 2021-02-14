736f19fb5dac0689931fdf568aa65be7e94ed07c
Madeline Ferguson <lusul101@gmail.com>
2021-02-13 22:52:22 -0700

    Initial rook tests

--- de1/test/generator-tests/test-rook-generator.c
+++ de1/test/generator-tests/test-rook-generator.c
@@ -4,6 +4,43 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+static void rook_test0w()
+{
+    board_t *board = init_board();
+
+    move_list_t exptected;
+    exptected.moves = NULL;
+    exptected.num_moves = 0;
+
+    move_list_t *actual = generate_rook_moves(board, WROOK0);
+
+    test_result_t result = board_list_equals(&exptected, actual);
+    print_test_result(result, __func__);
+
+    free(board);
+    free(actual);
+}
+
+static void rook_test0b()
+{
+    board_t *board = init_board();
+
+    move_list_t exptected;
+    exptected.moves = NULL;
+    exptected.num_moves = 0;
+
+    move_list_t *actual = generate_rook_moves(board, BROOK1);
+
+    test_result_t result = board_list_equals(&exptected, actual);
+    print_test_result(result, __func__);
+
+    free(board);
+    free(actual);
+}
+
 void test_rook_generator()
 {
+    rook_test0w();
+    rook_test0b();
 }
+
