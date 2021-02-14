f4563733eabc4712235062f91fa40910b45c8ee2
Madeline Ferguson <lusul101@gmail.com>
2021-02-13 22:42:20 -0700

    Add stubs for possible move generators

--- de1/possible-move-generators.c
+++ de1/possible-move-generators.c
@@ -5,3 +5,28 @@ move_list_t *generate_pawn_moves(board_t *current, int pawn)
 {
     return NULL;
 }
+
+move_list_t *generate_rook_moves(board_t *current, int rook)
+{
+    return NULL;
+}
+
+move_list_t *generate_knight_moves(board_t *current, int knight)
+{
+    return NULL;
+}
+
+move_list_t *generate_bishop_moves(board_t *current, int bishop)
+{
+    return NULL;
+}
+
+move_list_t *generate_queen_moves(board_t *current, int pawn)
+{
+    return NULL;
+}
+
+move_list_t *generate_king_moves(board_t *current, int pawn)
+{
+    return NULL;
+}
