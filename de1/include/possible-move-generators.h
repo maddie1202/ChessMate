0c10fb4a20d0aab60a77d19e530f40898e8293db
Madeline Ferguson <lusul101@gmail.com>
2021-02-13 22:51:37 -0700

    Add possible move generator function stubs to header

--- de1/include/possible-move-generators.h
+++ de1/include/possible-move-generators.h
@@ -9,5 +9,10 @@ typedef struct move_list {
 } move_list_t;
 
 move_list_t *generate_pawn_moves(board_t *current, int pawn);
+move_list_t *generate_rook_moves(board_t *current, int rook);
+move_list_t *generate_knight_moves(board_t *current, int knight);
+move_list_t *generate_bishop_moves(board_t *current, int bishop);
+move_list_t *generate_queen_moves(board_t *current, int pawn);
+move_list_t *generate_king_moves(board_t *current, int pawn);
 
 #endif
