7d11f8bad94e340a6f46bb3847540c8b3f060306
alyssadacosta <alyssa@thedacostas.ca>
2021-02-15 20:03:53 -0700

    Pawn upgrade test (don't have appropriate constants yet, will NOT make)

--- de1/test/generator-tests/test-pawn-generator.c
+++ de1/test/generator-tests/test-pawn-generator.c
@@ -11,6 +11,7 @@
  * TEST 2: Take an enemy piece
  * TEST 3: Pawn can only move ahead by one space if it has already moved from its' original spot
  * TEST 4: Pawn cannot take a piece that is the same colour as itself
+ * TEST 5: Tests pawn upgrades
  */
 
 /* TEST 0: 
@@ -295,15 +296,215 @@ static void pawn_test4()
     destroy_move_list(b_actual);
 }
 
+/* TEST 5:
+ * Test pawn upgrade to each type of piece (bishop, knight, rook, queen)
+ * Tests both black and white pawns
+ */
+static void pawn_test5()
+{
+    board_t curr = {
+            {WROOK0, WKNIGHT0, WBISHOP0, EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {WKING,  WQUEEN,   EMPTY,    EMPTY,  BPAWN4, BPAWN5,   BPAWN6,   BPAWN7},       
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {WPAWN0, WPAWN1,   WPAWN2,   WPAWN3, EMPTY,  EMPTY,    BQUEEN,   BKING},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  BBISHOP1, BKNIGHT1, BROOK1}
+        };
+
+    board_t wp0_expected1 = {
+            {WROOK0, WKNIGHT0, WBISHOP0, EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {WKING,  WQUEEN,   EMPTY,    EMPTY,  BPAWN4, BPAWN5,   BPAWN6,   BPAWN7},       
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  WPAWN1,   WPAWN2,   WPAWN3, EMPTY,  EMPTY,    BQUEEN,   BKING},
+            {WROOK2, EMPTY,    EMPTY,    EMPTY,  EMPTY,  BBISHOP1, BKNIGHT1, BROOK1}
+        };
+
+    move_list_t wp0_expected;
+    board_t* wp0_expected_moves[1] = {&wp0_expected1};
+    wp0_expected.moves = wp0_expected_moves;
+    wp0_expected.num_moves = 1;
+
+    // generate WPAWN0 moves (upgrade to rook)
+    move_list_t *wp0_actual = generate_pawn_moves(&curr, WPAWN0);
+
+    board_t wp1_expected1 = {
+            {WROOK0, WKNIGHT0, WBISHOP0, EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {WKING,  WQUEEN,   EMPTY,    EMPTY,  BPAWN4, BPAWN5,   BPAWN6,   BPAWN7},       
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {WPAWN0, EMPTY,    WPAWN2,   WPAWN3, EMPTY,  EMPTY,    BQUEEN,   BKING},
+            {EMPTY,  WKNIGHT3, EMPTY,    EMPTY,  EMPTY,  BBISHOP1, BKNIGHT1, BROOK1}
+        };
+    
+    move_list_t wp1_expected;
+    board_t* wp1_expected_moves[1] = {&wp1_expected1};
+    wp1_expected.moves = wp1_expected_moves;
+    wp1_expected.num_moves = 1;
+
+    // generate WPAWN1 moves (upgrade to knight)
+    move_list_t *wp1_actual = generate_pawn_moves(&curr, WPAWN1);
+
+    board_t wp2_expected1 = {
+            {WROOK0, WKNIGHT0, WBISHOP0, EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {WKING,  WQUEEN,   EMPTY,    EMPTY,  BPAWN4, BPAWN5,   BPAWN6,   BPAWN7},       
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {WPAWN0, WPAWN1,   EMPTY,    WPAWN3, EMPTY,  EMPTY,    BQUEEN,   BKING},
+            {EMPTY,  EMPTY,    WBISHOP4, EMPTY,  EMPTY,  BBISHOP1, BKNIGHT1, BROOK1}
+        };
+    
+    move_list_t wp2_expected;
+    board_t* wp2_expected_moves[1] = {&wp2_expected1};
+    wp2_expected.moves = wp2_expected_moves;
+    wp2_expected.num_moves = 1;
+
+    // generate WPAWN2 moves (upgrade to bishop)
+    move_list_t *wp2_actual = generate_pawn_moves(&curr, WPAWN2);
+
+    board_t wp3_expected1 = {
+            {WROOK0, WKNIGHT0, WBISHOP0, EMPTY,   EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {WKING,  WQUEEN,   EMPTY,    EMPTY,   BPAWN4, BPAWN5,   BPAWN6,   BPAWN7},       
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,   EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,   EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,   EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,   EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {WPAWN0, WPAWN1,   WPAWN2,   EMPTY,   EMPTY,  EMPTY,    BQUEEN,   BKING},
+            {EMPTY,  EMPTY,    EMPTY,    WQUEEN3, EMPTY,  BBISHOP1, BKNIGHT1, BROOK1}
+        };
+
+    move_list_t wp3_expected;
+    board_t* wp3_expected_moves[1] = {&wp3_expected1};
+    wp3_expected.moves = wp3_expected_moves;
+    wp3_expected.num_moves = 1;
+
+    // generate WPAWN3 moves (upgrade to queen)
+    move_list_t *wp3_actual = generate_pawn_moves(&curr, WPAWN3);
+
+    board_t bp4_expected1 = {
+            {WROOK0, WKNIGHT0, WBISHOP0, EMPTY,  BQUEEN4, EMPTY,    EMPTY,    EMPTY},
+            {WKING,  WQUEEN,   EMPTY,    EMPTY,  EMPTY,   BPAWN5,   BPAWN6,   BPAWN7},       
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,   EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,   EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,   EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,   EMPTY,    EMPTY,    EMPTY},
+            {WPAWN0, WPAWN1,   WPAWN2,   WPAWN3, EMPTY,   EMPTY,    BQUEEN,   BKING},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,   BBISHOP1, BKNIGHT1, BROOK1}
+        };
+
+    move_list_t bp4_expected;
+    board_t* bp4_expected_moves[1] = {&bp4_expected1};
+    bp4_expected.moves = bp4_expected_moves;
+    bp4_expected.num_moves = 1;
+
+    // generate BPAWN4 moves (upgrade to queen)
+    move_list_t *bp4_actual = generate_pawn_moves(&curr, BPAWN4);
+
+    board_t bp5_expected1 = {
+            {WROOK0, WKNIGHT0, WBISHOP0, EMPTY,  EMPTY,  BBISHOP7, EMPTY,    EMPTY},
+            {WKING,  WQUEEN,   EMPTY,    EMPTY,  BPAWN4, EMPTY,    BPAWN6,   BPAWN7},       
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {WPAWN0, WPAWN1,   WPAWN2,   WPAWN3, EMPTY,  EMPTY,    BQUEEN,   BKING},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  BBISHOP1, BKNIGHT1, BROOK1}
+        };
+
+    move_list_t bp5_expected;
+    board_t* bp5_expected_moves[1] = {&bp5_expected1};
+    bp5_expected.moves = bp5_expected_moves;
+    bp5_expected.num_moves = 1;
+
+    // generate BPAWN5 moves (upgrade to bishop)
+    move_list_t *bp5_actual = generate_pawn_moves(&curr, BPAWN5);
+
+    board_t bp6_expected1 = {
+            {WROOK0, WKNIGHT0, WBISHOP0, EMPTY,  EMPTY,  EMPTY,    BKNIGHT8, EMPTY},
+            {WKING,  WQUEEN,   EMPTY,    EMPTY,  BPAWN4, BPAWN5,   EMPTY,    BPAWN7},       
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {WPAWN0, WPAWN1,   WPAWN2,   WPAWN3, EMPTY,  EMPTY,    BQUEEN,   BKING},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  BBISHOP1, BKNIGHT1, BROOK1}
+        };
+
+    move_list_t bp6_expected;
+    board_t* bp6_expected_moves[1] = {&bp6_expected1};
+    bp6_expected.moves = bp6_expected_moves;
+    bp6_expected.num_moves = 1;
+
+    // generate BPAWN4 moves (upgrade to knight)
+    move_list_t *bp6_actual = generate_pawn_moves(&curr, BPAWN6);
+
+    board_t bp7_expected1 = {
+            {WROOK0, WKNIGHT0, WBISHOP0, EMPTY,  EMPTY,  EMPTY,    EMPTY,    BROOK9},
+            {WKING,  WQUEEN,   EMPTY,    EMPTY,  BPAWN4, BPAWN5,   BPAWN6,   EMPTY},       
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  EMPTY,    EMPTY,    EMPTY},
+            {WPAWN0, WPAWN1,   WPAWN2,   WPAWN3, EMPTY,  EMPTY,    BQUEEN,   BKING},
+            {EMPTY,  EMPTY,    EMPTY,    EMPTY,  EMPTY,  BBISHOP1, BKNIGHT1, BROOK1}
+        };
+
+    move_list_t bp7_expected;
+    board_t* bp7_expected_moves[1] = {&bp7_expected1};
+    bp7_expected.moves = bp7_expected_moves;
+    bp7_expected.num_moves = 1;
+
+    // generate BPAWN7 moves (upgrade to rook)
+    move_list_t *bp7_actual = generate_pawn_moves(&curr, BPAWN7);
+
+    // CMP boards
+    test_result_t wp0_result = board_list_equals(&wp0_expected, wp0_actual);
+    test_result_t wp1_result = board_list_equals(&wp1_expected, wp1_actual);
+    test_result_t wp2_result = board_list_equals(&wp2_expected, wp2_actual);
+    test_result_t wp3_result = board_list_equals(&wp3_expected, wp3_actual);
+    test_result_t bp4_result = board_list_equals(&bp4_expected, bp4_actual);
+    test_result_t bp5_result = board_list_equals(&bp5_expected, bp5_actual);
+    test_result_t bp6_result = board_list_equals(&bp6_expected, bp6_actual);
+    test_result_t bp7_result = board_list_equals(&bp7_expected, bp7_actual);
+
+    // display results
+    print_test_result(wp0_result, __func__);
+    print_test_result(wp1_result, __func__);
+    print_test_result(wp2_result, __func__);
+    print_test_result(wp3_result, __func__);
+    print_test_result(bp4_result, __func__);
+    print_test_result(bp5_result, __func__);
+    print_test_result(bp6_result, __func__);
+    print_test_result(bp7_result, __func__);
+
+    // free generated boards
+    destroy_move_list(wp0_actual);
+    destroy_move_list(wp1_actual);
+    destroy_move_list(wp2_actual);
+    destroy_move_list(wp3_actual);
+    destroy_move_list(bp4_actual);
+    destroy_move_list(bp5_actual);
+    destroy_move_list(bp6_actual);
+    destroy_move_list(bp7_actual);
+}
+
 /*
- * Run the 5 pawn tests
+ * Run the pawn tests
  */
 void test_pawn_generator()
 {
-    // call all 5 tests
     pawn_test0();
     pawn_test1();
     pawn_test2();
     pawn_test3();
     pawn_test4();
+    pawn_test5();  // fix calls to pawn generator once pawn upgrade is implemented
 }
