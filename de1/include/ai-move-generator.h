ac31755ef2590fc3432c9938e45c8238a31f49d3
Madeline Ferguson <lusul101@gmail.com>
2021-02-17 11:03:25 -0700

    Implement eval_board

--- de1/include/ai-move-generator.h
+++ de1/include/ai-move-generator.h
@@ -2,3 +2,6 @@
 
 bool in_check(board_t *board, int colour);
 bool in_checkmate(board_t *board, int colour);
+board_t *generate_ai_move(game_t *game, int colour, int depth);
+
+double eval_board(board_t *board, int colour);
