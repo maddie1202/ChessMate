9d5320fd7355a07e843f3929d3637b9f585cdeee
emilyl <emily.lukas@gmail.com>
2021-02-24 14:40:59 -0800

    add basic structures for chessboard

---
+++ frontend/ChessieGame/app/src/main/java/com/example/chessiegame/components/Board.java
@@ -0,0 +1,9 @@
+package com.example.chessiegame.components;
+
+import com.example.chessiegame.components.Tile;
+import java.util.List;
+
+public class Board {
+    public List<List<Tile>> board;
+    
+}
